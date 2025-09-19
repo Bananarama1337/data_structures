67328385319f355e2064ada43d6475d0fd8eda6b
Alexey <Aleksey.Kalyuzhnyy@mechatronica-pro.com>
2025-09-19 12:21:29 +0700

    Added copy control functions & remove & traversals

--- binary_tree.h
+++ binary_tree.h
@@ -11,35 +11,94 @@ struct TreeNode {
 
 template<typename T>
 class binary_tree {
-    TreeNode<T>* root = nullptr;
+private:
+    TreeNode<T>* root_ = nullptr;
 
     TreeNode<T>* insert_rec(TreeNode<T>* node, const T& value);
     TreeNode<T>* find_rec(TreeNode<T>* node, const T& value) const;
+    TreeNode<T>* remove_rec(TreeNode<T>* node, const T& value);
+    TreeNode<T>* find_min(TreeNode<T>* node) const;
+    void preorder_rec(TreeNode<T>* node, std::vector<T>&) const;
+    void postorder_rec(TreeNode<T>* node, std::vector<T>&) const;
+    void inorder_rec(TreeNode<T>* node, std::vector<T>& result) const;
+    TreeNode<T>* copy_rec(TreeNode<T>* node) const;
     
     template<typename U>
     friend void print(binary_tree<U>& tree);
 
     template<typename U>
     friend void print_rec(TreeNode<U>* node, std::size_t depth);
+
 public:
     binary_tree(const T& value = T()) {
-        root = new TreeNode<T>(value);
-    };
+        root_ = new TreeNode<T>(value);
+    }
 
     binary_tree(const binary_tree& other);
-    binary_tree(binary_tree&& other);
+    binary_tree(binary_tree&& other) noexcept;
     binary_tree& operator=(const binary_tree& other);
-    binary_tree& operator=(binary_tree&& other);
+    binary_tree& operator=(binary_tree&& other) noexcept;
 
     ~binary_tree() {
-        clear(root);
+        clear(root_);
     }
 
     void clear(TreeNode<T>* node) noexcept;
     void insert(const T& value);
     void remove(const T& value);
     TreeNode<T>* find(const T& value);
+
+    std::vector<T> preorder_traversal() const;
+    std::vector<T> postorder_traversal() const;
+    std::vector<T> inorder_traversal() const;
 };
+
+template<typename T>
+binary_tree<T>::binary_tree(const binary_tree& other) {
+    root_ = copy_rec(other.root_);
+}
+
+template<typename T>
+TreeNode<T>* binary_tree<T>::copy_rec(TreeNode<T>* node) const {
+    if (!node) return nullptr;
+
+    TreeNode<T>* newNode = new TreeNode<T>(node->data);
+    newNode->left  = copy_rec(node->left);
+    newNode->right = copy_rec(node->right);
+
+    return newNode;
+}
+
+template<typename T>
+binary_tree<T>::binary_tree(binary_tree&& other) noexcept {
+    root_ = other.root_;
+    other.root_ = nullptr;
+}
+
+template<typename T>
+binary_tree<T>& binary_tree<T>::operator=(const binary_tree& other) {
+    if (this == &other) {
+        return *this;
+    }
+
+    clear(root_);
+    root_ = copy_rec();
+
+    return *this;
+}
+
+template<typename T>
+binary_tree<T>& binary_tree<T>::operator=(binary_tree&& other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+
+    clear(root_);
+    root_ = other.root_;
+    other.root_ = nullptr;
+
+    return *this;
+}
 
 template<typename T>
 void binary_tree<T>::clear(TreeNode<T>* node) noexcept {
@@ -54,7 +113,7 @@ void binary_tree<T>::clear(TreeNode<T>* node) noexcept {
 
 template<typename T>
 void binary_tree<T>::insert(const T& value) {
-    root = insert_rec(root, value);
+    root_ = insert_rec(root_, value);
 }
 
 template<typename T>
@@ -75,17 +134,57 @@ TreeNode<T>* binary_tree<T>::insert_rec(TreeNode<T>* node, const T& value) {
 
 template<typename T>
 void binary_tree<T>::remove(const T& value) {
-    TreeNode<T>* node = find(value);
-    if (!node) {
-        return;
+    TreeNode<T>* node = remove_rec(root_, value);
+}
+
+template<typename T>
+TreeNode<T>* binary_tree<T>::remove_rec(TreeNode<T>* node, const T& value) {
+    if (!node) return nullptr;
+
+    if (value < node->data) {
+        node->left = remove_rec(node->left, value);
+    } 
+    else if (value > node->data) {
+        node->right = remove_rec(node->right, value);
+    } 
+    else {
+        if (!node->left && !node->right) {
+            // случай 1: лист
+            delete node;
+            return nullptr;
+        } 
+        else if (!node->left) {
+            // случай 2: только правый потомок
+            TreeNode<T>* temp = node->right;
+            delete node;
+            return temp;
+        } 
+        else if (!node->right) {
+            // случай 2: только левый потомок
+            TreeNode<T>* temp = node->left;
+            delete node;
+            return temp;
+        } 
+        else {
+            // случай 3: два потомка
+            TreeNode<T>* successor = find_min(node->right);
+            node->data = successor->data;
+            node->right = remove_rec(node->right, successor->data);
+        }
     }
 
-    return;
+    return node;
+}
+
+template<typename T>
+TreeNode<T>* binary_tree<T>::find_min(TreeNode<T>* node) const {
+    while (node && node->left) node = node->left;
+    return node;
 }
 
 template<typename T>
 TreeNode<T>* binary_tree<T>::find(const T& value) {
-    return find_rec(root, value);
+    return find_rec(root_, value);
 }
 
 template<typename T>
@@ -105,8 +204,69 @@ TreeNode<T>* binary_tree<T>::find_rec(TreeNode<T>* node, const T& value) const {
 }
 
 template<typename T>
+std::vector<T> binary_tree<T>::preorder_traversal() const {
+    std::vector<T> result;
+    preorder_rec(root_, result);
+
+    return result;
+}
+
+template<typename T>
+void binary_tree<T>::preorder_rec(TreeNode<T>* node, std::vector<T>& result) const {
+    if (!node) { 
+        return; 
+    }
+
+    result.push_back(node->data);
+
+    preorder_rec(node->left, result);
+    preorder_rec(node->right, result);
+}
+
+template<typename T>
+std::vector<T> binary_tree<T>::postorder_traversal() const {
+    std::vector<T> result;
+    postorder_rec(root_, result);
+
+    return result;
+}
+
+template<typename T>
+void binary_tree<T>::postorder_rec(TreeNode<T>* node, std::vector<T>& result) const {
+    if (!node) { 
+        return; 
+    }
+
+    postorder_rec(node->left, result);
+    postorder_rec(node->right, result);
+
+    result.push_back(node->data);
+}
+
+template<typename T>
+std::vector<T> binary_tree<T>::inorder_traversal() const {
+    std::vector<T> result;
+    inorder_rec(root_, result);
+
+    return result;
+}
+
+template<typename T>
+void binary_tree<T>::inorder_rec(TreeNode<T>* node, std::vector<T>& result) const {
+    if (!node) { 
+        return; 
+    }
+
+    inorder_rec(node->left, result);
+
+    result.push_back(node->data);
+
+    inorder_rec(node->right, result);
+}
+
+template<typename T>
 void print(binary_tree<T>& tree) {
-    print_rec(tree.root, 0);
+    print_rec(tree.root_, 0);
 }
 
 template<typename T>
